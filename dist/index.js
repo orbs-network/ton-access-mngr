f5fa446a642f477a7e5411ce7b80d3d8bee17ac0
uvorbs <yuval@orbs.com>
2023-01-25 15:34:54 +0200

    test2

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // Import the express in typescript file
-import express from 'express';
-
+const express_1 = __importDefault(require("express"));
 // Initialize the express engine
-const app: express.Application = express();
-
+const app = (0, express_1.default)();
 // Take a port 3000 for running server.
-const port: number = 3000;
-
+const port = 3000;
 // Handling '/' Request
 app.get('/', (_req, _res) => {
     _res.send("TypeScript With Express");
 });
-
 // Server setup
 app.listen(port, () => {
     console.log(`TypeScript with Express
