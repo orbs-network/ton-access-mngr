11cf3c12c85b4f866f2a6b3147104c76c956859b
uvorbs <yuval@orbs.com>
2023-02-15 09:44:07 +0200

    change json

--- src/mngr.ts
+++ src/mngr.ts
@@ -1,7 +1,18 @@
+import { BlobOptions } from "buffer";
 import { MonitorActivity } from "./v2-ton-client"
 
+// from ton-access lib
+// type EdgeProtocol = "toncenter-api-v2" | "ton-api-v4" | "adnl-proxy"; // default: toncenter-api-v2
+// type Network = "mainnet" | "testnet"; //| "sandbox"- is deprecated ; // default: mainnet
+//type ProtoNet = "v2-mainnet" | "v2-testnet" | "v4-mainnet" | "v4-testnet";  //| "sandbox"- is deprecated ; // 
+type ProtoNetHealth = {
+    "v2-mainnet": boolean,
+    "v2-testnet": boolean,
+    "v4-mainnet": boolean,
+    "v4-testnet": boolean
+}
 export class Mngr {
-    v2Mainnet: boolean;
+    health: ProtoNetHealth | undefined;
     successTS: number;
     errors: Array<string>;
     status: any;
@@ -9,7 +20,6 @@ export class Mngr {
     constructor() {
         this.successTS = -1;
         this.errors = [];
-        this.v2Mainnet = false;
         const dt = new Date();
         this.status = {
             updated: dt.toUTCString(),
@@ -22,11 +32,16 @@ export class Mngr {
         setTimeout(this.runLoop.bind(this), 60 * 1000)
     }
     async monitor() {
-        // reset 
-        this.v2Mainnet = false;
+        // reset         
         this.errors = [];
+        this.health = {
+            "v2-mainnet": false,
+            "v2-testnet": false,
+            "v4-mainnet": false,
+            "v4-testnet": false
+        }
         try {
-            this.v2Mainnet = await this.Checkv2Mainnet();
+            this.health['v2-mainnet'] = await this.Checkv2Mainnet();
             this.successTS = Date.now();
         }
         catch (e: any) {
@@ -65,7 +80,7 @@ export class Mngr {
         const dt = new Date();
         this.status = {
             updated: dt.toUTCString(),
-            v2Mainnet: this.v2Mainnet,
+            health: this.health,
             successTS: this.successTS,
             errors: this.errors,
             code: code,
