64205e2a2f5d542fc250672d241994f24331fda4
uvorbs <yuval@orbs.com>
2023-06-15 13:56:25 +0300

    promote 1.0.2

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 git pull
 docker build -t ton-access-mngr .
-docker tag      ton-access-mngr orbsnetwork/ton-access-mngr:v1.0.0
-docker push     orbsnetwork/ton-access-mngr:v1.0.0
+docker tag      ton-access-mngr orbsnetwork/ton-access-mngr:v1.0.2
+docker push     orbsnetwork/ton-access-mngr:v1.0.2
+
+# v1.0.2
+# make everything serial
