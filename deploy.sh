e2fdc64385742b0bbe077cce7da032c9694bec43
uvorbs <yuval@orbs.com>
2023-08-31 12:10:48 +0300

    align with version

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 git pull
 docker build -t ton-access-mngr .
-docker tag      ton-access-mngr orbsnetwork/ton-access-mngr:v1.0.2
-docker push     orbsnetwork/ton-access-mngr:v1.0.2
-
-# v1.0.2
-# make everything serial
+docker tag      ton-access-mngr orbsnetwork/ton-access-mngr:v1.1.0
+docker push     orbsnetwork/ton-access-mngr:v1.1.0
