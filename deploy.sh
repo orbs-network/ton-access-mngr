6296d2ccf8b40b1232b9f1520908b244ac222df3
uvorbs <yuval@orbs.com>
2023-12-17 10:57:32 +0200

    deploy docker

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
 docker build -t ton-access-mngr .
-docker tag      ton-access-mngr orbsnetwork/ton-access-mngr:v1.1.0
-docker push     orbsnetwork/ton-access-mngr:v1.1.0
+docker tag      ton-access-mngr orbsnetwork/ton-access-mngr:v1.1.1
+docker push     orbsnetwork/ton-access-mngr:v1.1.1
